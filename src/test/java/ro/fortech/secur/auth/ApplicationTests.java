8a2c1b0a9ba15bf22ef1c36a621dcb4e926b4364
MagdalenaMadalina <mada.magdalena97@gmail.com>
2020-01-31 11:44:02 +0200

    [SEC-5] - Create secur-auth microservice project in Intellij
    Created project stucture

---
+++ src/test/java/ro/fortech/secur/auth/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.fortech.secur.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
