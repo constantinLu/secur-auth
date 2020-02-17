a779ea19dcfa434617fa5c4f5896a7c4a0397d6d
gizet <lunguucatalin@gmail.com>
2020-02-17 11:03:26 +0200

    [SEC-37] Error Mapping and logging

--- src/main/java/ro/secur/auth/exceptions/ExceptionControllerAdvice.java
+++ src/main/java/ro/secur/auth/exceptions/ExceptionControllerAdvice.java
@@ -1,26 +1,26 @@
 package ro.secur.auth.exceptions;
 
-        import lombok.extern.slf4j.Slf4j;
-        import org.springframework.http.HttpStatus;
-        import org.springframework.http.ResponseEntity;
-        import org.springframework.web.bind.annotation.ControllerAdvice;
-        import org.springframework.web.bind.annotation.ExceptionHandler;
-        import ro.secur.auth.exceptions.custom.UserNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import ro.secur.auth.exceptions.custom.UserNotFoundException;
 
 @ControllerAdvice
 @Slf4j
 public class ExceptionControllerAdvice {
 
-    private final ResponseFactory response;
+    private final ResponseBuilder response;
 
-    public ExceptionControllerAdvice(ResponseFactory response) {
+    public ExceptionControllerAdvice(ResponseBuilder response) {
         this.response = response;
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity handleUserNotFoundException(final UserNotFoundException e) {
         log.error(e.getMessage());
-        return response.create(e, HttpStatus.BAD_REQUEST);
+        return response.error(e, HttpStatus.BAD_REQUEST);
     }
 
 }
