cbd9fa4ef6401184b97df953a585fcfe9c0435d8
gizet <lunguucatalin@gmail.com>
2020-02-21 18:05:15 +0200

    [SEC-47] Change pass - 4th commit

--- src/main/java/ro/secur/auth/controller/UserController.java
+++ src/main/java/ro/secur/auth/controller/UserController.java
@@ -4,6 +4,7 @@ package ro.secur.auth.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ro.secur.auth.dto.UserDto;
@@ -30,7 +31,7 @@ public class UserController {
         return userService.getAllUsers();
     }
 
-    @PostMapping("/password")
+    @PutMapping("/user/username")
     public void changePassword(@RequestBody ChangePasswordRequest request) {
         userService.changePassword(request);
     }
