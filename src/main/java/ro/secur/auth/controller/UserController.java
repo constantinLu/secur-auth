c88fdd0bec3d1eaf64fcfed7e7cd5d07b7f6335f
gizet <lunguucatalin@gmail.com>
2020-02-24 13:43:39 +0200

    [SEC-47] Change pass - 6th commit

--- src/main/java/ro/secur/auth/controller/UserController.java
+++ src/main/java/ro/secur/auth/controller/UserController.java
@@ -4,7 +4,6 @@ package ro.secur.auth.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
--- src/test/java/ro/secur/auth/controller/LoginControllerTest.java
+++
@@ -1,7 +0,0 @@
-package ro.secur.auth.controller;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class LoginControllerTest {
-
-}
