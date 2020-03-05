93e0510533741e05a0f42a6598f9cd62d2cbf55a
Cosmina Iacob <cosmina.iacob94@gmail.com>
2020-03-05 20:13:14 +0200

    Feature/sec 66 Register endpoint (#22)
    * [SEC-64] fixed merge
    
    * [SEC-66] Added endpoint for register user
    
    * [SEC-66] Added endpoint for register user

--- src/main/java/ro/secur/auth/entity/UserEntity.java
+++ src/main/java/ro/secur/auth/entity/UserEntity.java
@@ -1,21 +1,8 @@
 package ro.secur.auth.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Set;
 
 @Getter
@@ -37,7 +24,7 @@ public class UserEntity {
 
     private String resetToken;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "USER_INFO_ID")
     private UserInfoEntity userInfoEntity;
 
--- src/test/java/ro/secur/auth/service/UserServiceTest.java
+++ src/test/java/ro/secur/auth/service/UserServiceTest.java
@@ -16,6 +16,7 @@ import ro.secur.auth.entity.UserEntity;
 import ro.secur.auth.exceptions.custom.InvalidPasswordException;
 import ro.secur.auth.exceptions.custom.PasswordMisMatchException;
 import ro.secur.auth.exceptions.custom.UserNotFoundException;
+import ro.secur.auth.repository.RoleRepository;
 import ro.secur.auth.repository.UserInfoRepository;
 import ro.secur.auth.repository.UserRepository;
 import ro.secur.auth.security.password.ChangePasswordRequest;
@@ -36,6 +37,9 @@ class UserServiceTest {
     private UserRepository userRepository;
 
     @Mock
+    private RoleRepository roleRepository;
+
+    @Mock
     private UserInfoRepository userInfoRepository;
 
     @Mock
@@ -47,7 +51,7 @@ class UserServiceTest {
 
     @BeforeEach
     void setUp() {
-        userService = new UserService(userRepository, userInfoRepository, new ModelMapper(), passwordConfiguration, emailService);
+        userService = new UserService(userRepository, userInfoRepository, roleRepository, new ModelMapper(), passwordConfiguration, emailService);
     }
 
 
