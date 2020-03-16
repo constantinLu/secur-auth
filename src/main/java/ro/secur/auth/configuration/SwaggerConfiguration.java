e62c2ecaed33d30458d41aa8a40b437b0d3c8e04
Lungu Constantin <gizet_st@outlook.com>
2020-03-16 20:36:55 +0200

    [SEC-87] Swagger Impl #1 try

--- src/main/java/ro/secur/auth/configuration/SwaggerConfiguration.java
+++ src/main/java/ro/secur/auth/configuration/SwaggerConfiguration.java
@@ -3,38 +3,57 @@ package ro.secur.auth.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import static springfox.documentation.builders.PathSelectors.regex;
+import static io.swagger.models.auth.In.HEADER;
+import static java.util.Collections.singletonList;
+import static javax.ws.rs.core.HttpHeaders.AUTHORIZATION;
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
+
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
 
+
     @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
+    public Docket api() {
+        return new Docket(SWAGGER_2)
+                .apiInfo(apiInfo())
+                .securitySchemes(singletonList(new ApiKey("JWT", AUTHORIZATION, HEADER.name())))
+                .securityContexts(singletonList(
+                        SecurityContext.builder()
+                                .securityReferences(
+                                        singletonList(SecurityReference.builder()
+                                                .reference("JWT")
+                                                .scopes(new AuthorizationScope[0])
+                                                .build()
+                                        )
+                                )
+                                .build())
+                )
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
+                .apis(RequestHandlerSelectors.basePackage("ro.secur.auth"))
+                .build();
     }
 
+
     private ApiInfo apiInfo() {
-        Contact contact = new Contact("Adam Zaręba", "http://adamzareba.github.io", "testEmail@gmail.com");
+        Contact contact = new Contact("Fortech Secur", "https://github.com/gizet/secur-auth", "fortechSecur@gmail.com");
         return new ApiInfoBuilder()
-                .title("Company Application REST API")
-                .description("List of available API served by Company Application")
+                .title("Secur App REST API")
+                .description("List of available API served by secur App")
                 .version("1.0")
-                .license("Apache 2.0")
-                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                .license("None")
+                .licenseUrl("")
                 .contact(contact)
                 .build();
     }
