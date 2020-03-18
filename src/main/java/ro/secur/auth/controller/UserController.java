d60c8e39907996711d58c50686ff55a4ca4c5554
beatriced <beatricedtoma@gmail.com>
2020-03-18 15:59:06 +0200

    Feature/sec 83 (#33)
    * [SEC-83] Roles functionality - BE + FE

--- src/main/java/ro/secur/auth/controller/UserController.java
+++ src/main/java/ro/secur/auth/controller/UserController.java
@@ -1,6 +1,5 @@
 package ro.secur.auth.controller;
 
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
